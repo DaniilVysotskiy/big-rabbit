5c6ed980f2ed035ef72e847f0620527f9c68a800
cherezzabo <cherezzabo@gmail.com>
2017-08-31 20:18:16 +0300

    wip

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,6 +1,63 @@
 import svg4everybody from 'svg4everybody';
 import $ from 'jquery';
+import ymaps from 'ymaps';
+import mask from 'jquery-mask-plugin';
+
 
 $(() => {
+
+	const docWidth = document.documentElement.offsetWidth;
+	[].forEach.call(
+		document.querySelectorAll('*'),
+		function (el) {
+			if (el.offsetWidth > docWidth) {
+				console.log(el);
+			}
+		}
+	);
+
+	$('input[type="tel"]').mask('+7 (000) 000-00-00');
+
+	ymaps.load()
+		.then(maps => {
+			/**
+			 * Создаем мультимаршрут.
+			 * Первым аргументом передаем модель либо объект описания модели.
+			 * Вторым аргументом передаем опции отображения мультимаршрута.
+			 * @see https://api.yandex.ru/maps/doc/jsapi/2.1/ref/reference/multiRouter.MultiRoute.xml
+			 * @see https://api.yandex.ru/maps/doc/jsapi/2.1/ref/reference/multiRouter.MultiRouteModel.xml
+			 */
+			const multiRoute = new maps.multiRouter.MultiRoute({
+				// Описание опорных точек мультимаршрута.
+				referencePoints: [
+					'Москва',
+					'Юхнов'
+				],
+				// Параметры маршрутизации.
+				params: {
+					// Ограничение на максимальное количество маршрутов, возвращаемое маршрутизатором.
+					results: 2
+				}
+			}, {
+				// Автоматически устанавливать границы карты так, чтобы маршрут был виден целиком.
+				boundsAutoApply: true
+			});
+
+			// Создаем карту с добавленными на нее кнопками.
+			const myMap = new maps.Map('contactsYmap', {
+				center: [55.750625, 37.626],
+				zoom: 7,
+				controls: [],
+				behaviors: []
+			});
+
+			// Отключаем Scroll
+			myMap.behaviors.disable('scrollZoom');
+
+			// Добавляем мультимаршрут на карту.
+			myMap.geoObjects.add(multiRoute);
+		})
+		.catch(error => console.log('Failed to load Yandex Maps', error));
+
 	svg4everybody();
 });
