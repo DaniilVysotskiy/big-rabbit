a6a8b6df04cdb5aa07c25e9a9c59ac733752d394
cherezzabo <cherezzabo@gmail.com>
2017-09-09 18:06:32 +0300

    release version

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -4,56 +4,93 @@ import ymaps from 'ymaps';
 import mask from 'jquery-mask-plugin';
 import slick from 'slick-carousel';
 
-$(() => {
-
-	// const docWidth = document.documentElement.offsetWidth;
-	// [].forEach.call(
-	// 	document.querySelectorAll('*'),
-	// 	function (el) {
-	// 		if (el.offsetWidth > docWidth) {
-	// 			console.log(el);
-	// 		}
-	// 	}
-	// );
-
-
-	// Modals
-	function initModals() {
-	// Show modal
-		$('[data-link="modal"]').on('click', function (){
-			const link = $(this).attr('href') || $(this).data('href');
-			const modal = $(link);
-
-			$(modal).addClass('active');
-			if ($(modal).hasClass('modal--breed')){
-				$('.single-item').slick({
-					dots: false,
-					infinite: false,
-					nextArrow: '<button type="button" class="slick-next"></button>',
-					prevArrow: '<button type="button" class="slick-prev"></button>'
-				});
-			}
-			$('body').addClass('modal-is-active');
-		});
-		// Hide modal on cross click
-		$('.modal').on('click', '.close', function (){
-			$(this).closest('.modal--wrapper').removeClass('active');
-			$('body').removeClass('modal-is-active');
-		});
-		// Hide modal on empty place click
-		$(document).on('click', function (e){
-			const target = e.target;
-
-			if ($(target).hasClass('modal--wrapper')){
-				$('.modal--wrapper').removeClass('active');
-				$('body').removeClass('modal-is-active');
-			}
+// Slick slider init
+function slickInit() {
+	if ($('.single-item').length !== 0) {
+		$('.single-item').slick({
+			dots: false,
+			infinite: false,
+			nextArrow: '<button type="button" class="slick-next"></button>',
+			prevArrow: '<button type="button" class="slick-prev"></button>'
 		});
 	}
+}
 
-	// Masked phone input
-	$('input[type="tel"]').mask('+7 (000) 000-00-00');
+// Nanogallery2 init
+function nanogalleryInit() {
+	if ($('.nano2').length !== 0) {
+		$('.nano2').nanogallery2({
+			itemsBaseURL: '',
+			thumbnailWidth: '270',
+			thumbnailHeight: '270',
+			thumbnailBorderVertical: 0,
+			thumbnailBorderHorizontal: 0,
+			thumbnailLabel: {
+				display: false
+			},
+			thumbnailHoverEffect2: 'imageScaleIn80',
+			// galleryDisplayMode: 'moreButton',
+			galleryLastRowFull: true,
+			galleryMaxItems: 4,
+			thumbnailGutterWidth: 30,
+			thumbnailGutterHeight: 30,
+			gallerySorting: 'random',
+			displayBreadcrumb: false,
+			breadcrumbAutoHideTopLevel: false,
+			breadcrumbOnlyCurrentLevel: false
+		});
+	}
+}
 
+function openModal(modal){
+	$(modal).addClass('active');
+	if ($(modal).hasClass('modal--breed')){
+		$('.single-item').slick({
+			dots: false,
+			infinite: false,
+			nextArrow: '<button type="button" class="slick-next"></button>',
+			prevArrow: '<button type="button" class="slick-prev"></button>'
+		});
+	}
+}
+
+// Modals
+function initModals() {
+	// Show modal
+	$('[data-link="modal"]').on('click', function (){
+		const link = $(this).attr('href') || $(this).data('href');
+		const modal = $(link);
+
+		openModal(modal);
+	});
+	// Hide modal on cross click
+	$('.modal').on('click', '.close', function (){
+		$(this).closest('.modal--wrapper').removeClass('active');
+	});
+	// Hide modal on empty place click
+	$(document).on('click', function (e){
+		const target = e.target;
+
+		if ($(target).hasClass('modal--wrapper')){
+			$('.modal--wrapper').removeClass('active');
+		}
+	});
+	// Modal auto open with hash in URL
+	if (window.location.hash) {
+		const hash = window.location.hash;
+		openModal(hash);
+	}
+}
+
+// Masked phone input init
+function maskedInputInit() {
+	if ($('input[type="tel"]').length !== 0) {
+		$('input[type="tel"]').mask('+7 (000) 000-00-00');
+	}
+}
+
+// Yandex Maps init
+function ymapsInit() {
 	// Load Yandex.Map
 	if ($('#ymap').length !== 0) {
 		ymaps.load('https://api-maps.yandex.ru/2.1/?lang=ru_RU')
@@ -97,7 +134,14 @@ $(() => {
 			})
 			.catch(error => console.log('Failed to load Yandex Maps', error));
 	}
-
+}
+$(document).ready(function (){
 	svg4everybody();
 	initModals();
+	nanogalleryInit();
+	slickInit();
+	maskedInputInit();
+	ymapsInit();
 });
+
+
